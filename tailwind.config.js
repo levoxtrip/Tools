b36974bbb336d981847b74c6b60ff2dc0231ee5f
levoxtrip <levoxtrip@gmail.com>
2025-04-06 18:14:11 +0200

    further infos

--- tailwind.config.js
+++ tailwind.config.js
@@ -8,5 +8,13 @@ export default {
       },
     },
   },
+  // Set the screens for responsive design
+  screens: {
+    sm: "640px",
+    md: "768px",
+    lg: "1024px",
+    xl: "1280px",
+    "2xl": "1536px",
+  },
   plugins: [],
 };
