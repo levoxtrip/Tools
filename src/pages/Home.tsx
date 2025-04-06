b36484f18df6150d3b5496b46dba36d1345037e6
levoxtrip <levoxtrip@gmail.com>
2025-04-06 19:11:45 +0200

    new title

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -3,7 +3,7 @@ import CategoriesPage from "./CategoriesPage";
 const Home = () => {
   return (
     <div>
-      <h1>Home</h1>
+      <h1>Daily Tools To Strive</h1>
       <CategoriesPage />
     </div>
   );
